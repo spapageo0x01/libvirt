9b22316956445230267bc461b6300d49e75698aa
Spyros Papageorgiou <spyros@iofabric.com>
2017-05-11 15:56:52 +0300

    Helper script for package installation

---
+++ install_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yum -y install libpciaccess-devel libtool patch gettext-devel libnl-devel libxml2-devel device-mapper-devel
+
+yum -y install kvm virt-manager virt-install qemu-kvm xauth dejavu-lgc-sans-fonts virt-viewer
